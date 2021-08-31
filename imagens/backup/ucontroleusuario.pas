72794bbed169e1d80b77f48cf75e82c70b66475c
Fabricio Calvete campos <fabricio_calvete@hotmail.com>
2021-08-31 20:27:43 -0300

    Add files via upload

---
+++ imagens/backup/ucontroleusuario.pas
@@ -0,0 +1,30 @@
+unit uControleUsuario;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+
+type
+
+  { TformControleAcesso }
+
+  TformControleAcesso = class(TForm)
+    Image1: TImage;
+  private
+
+  public
+
+  end;
+
+var
+  formControleAcesso: TformControleAcesso;
+
+implementation
+
+{$R *.lfm}
+
+end.
+

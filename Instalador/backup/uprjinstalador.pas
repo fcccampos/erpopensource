a1e60e1d552965010596f893f4a2d6319bf06e7d
Fabricio Calvete campos <fabricio_calvete@hotmail.com>
2021-09-13 19:04:14 -0300

    Instalador
    Colocando as regras para criação de componentes em tempo real

--- Instalador/backup/uprjinstalador.pas
+++ Instalador/backup/uprjinstalador.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  ComCtrls, Grids, Types;
+  ComCtrls;
 
 type
 
@@ -16,8 +16,11 @@ type
     btnGerarScript: TButton;
     btnGerarScript1: TButton;
     Button1: TButton;
+    Button2: TButton;
     chkNulo01: TCheckBox;
+    cmbTabela1: TComboBox;
     cmbVariavel01: TComboBox;
+    cmbTabela: TComboBox;
     edtcampo01: TEdit;
     edtDecimal01: TEdit;
     edtNomeTabela: TEdit;
@@ -25,20 +28,28 @@ type
     edttamanho01: TEdit;
     Label1: TLabel;
     Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
     Label7: TLabel;
     lblCampo01: TLabel;
     lblDecimal01: TLabel;
     lblTamanho01: TLabel;
     lblVariavel01: TLabel;
+    ListBox1: TListBox;
+    ListBox2: TListBox;
     memoFirebird: TMemo;
     memoMySQL: TMemo;
     memoSQLite: TMemo;
     PageControl1: TPageControl;
     PageControl2: TPageControl;
+    RadioGroup1: TRadioGroup;
     TabSheet1: TTabSheet;
-    TabSheet2: TTabSheet;
+    tabCampoDaTabela: TTabSheet;
+    TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
     procedure btnGerarScriptClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure PageControl1Change(Sender: TObject);
   private
 
   public
@@ -77,19 +88,34 @@ end;
 procedure TfrmInstaladorSistema.Button1Click(Sender: TObject);
 var
   EditCampoTempoReal : TEdit;
+  intAlturaTop, I : Integer;
+
 begin
-  // Gerando campos em tempo de execução
-  EditCampoTempoReal := TEdit.Create(nil);
-  EditCampoTempoReal.Parent := TabSheet1;
-  EditCampoTempoReal.Height := 23;
-  EditCampoTempoReal.Width := 192;
-  EditCampoTempoReal.Top := 155;
-  EditCampoTempoReal.Left := 20;
-  EditCampoTempoReal.Name := 'edtCampo02';
-  EditCampoTempoReal.Caption := '';
-  EditCampoTempoReal.CharCase := ecLowerCase;
-  EditCampoTempoReal.MaxLength := 20;
-  EditCampoTempoReal.TabOrder := 3;
+  intAlturaTop := edtCampo01.top + 26;
+  I :=0;
+
+  While I < StrToInt(edtquantidadecampos.Text) Do Begin
+     // Gerando campos em tempo de execução
+     EditCampoTempoReal := TEdit.Create(nil);
+     EditCampoTempoReal.Parent := tabCampoDaTabela;
+     EditCampoTempoReal.Height := 23;
+     EditCampoTempoReal.Width := 192;
+     EditCampoTempoReal.Top := intAlturaTop;
+     EditCampoTempoReal.Left := 8;
+     EditCampoTempoReal.Name := 'edtCampo02';
+     EditCampoTempoReal.Caption := '';
+     EditCampoTempoReal.CharCase := ecLowerCase;
+     EditCampoTempoReal.MaxLength := 20;
+     EditCampoTempoReal.TabOrder := 3;
+     I := I + 1;
+     intAlturaTop := intAlturaTop + 26;
+  End;
+
+end;
+
+procedure TfrmInstaladorSistema.PageControl1Change(Sender: TObject);
+begin
+
 end;
 
 
